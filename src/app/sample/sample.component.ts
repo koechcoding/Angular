a3ac6aa7caac325424811413bd6f4ef698308149
KelvinKoech <koechkelvin2001@gmail.com>
2023-02-27 12:56:02 +0300

    sample data

---
+++ src/app/sample/sample.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-sample',
+  templateUrl: './sample.component.html',
+  styleUrls: ['./sample.component.css']
+})
+export class SampleComponent {
+
+}
